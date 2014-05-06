c094696822e6d93a352cf458831b36aad4977653
Sebastian Heath <sebastian.heath@gmail.com>
2014-05-05 21:22:56 -0400

    converter tweaking

--- convert_to_various.sh
+++ convert_to_various.sh
@@ -1,9 +1,13 @@
 # rdf turtle
+echo curl
 curl 'http://rdf.greggkellogg.net/distiller?format=turtle&in_fmt=jsonld&uri=http://sfsheath.github.io/roman-amphitheaters/roman-amphitheaters.geojson' > roman-amphitheaters.ttl
 
-rapper -i turtle roman-amphitheaters.ttl > roman-amphitheaters.rdf
+echo rapper
+rapper -i turtle roman-amphitheaters.ttl > roman-amphitheaters.n3
 
-~/Documents/apache-jena-2.11.1/bin/arq --query sparql/slider.sparql --data roman-amphitheaters.rdf  --results CSV > ra.csv
+echo arq
+~/Documents/apache-jena-2.11.1/bin/arq --query sparql/slider.sparql --data roman-amphitheaters.n3  --results CSV > ra.csv
 
 # KML
+echo ogr2ogr
 ogr2ogr -f KML roman-amphitheaters.kml roman-amphitheaters.geojson
